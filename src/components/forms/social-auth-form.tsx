cc677448efff02582e2f16a18a2505e857d0a2a1
Awaliyatul Hikmah <imadlz15@gmail.com>
2024-11-09 20:40:57 +0700

    implement google OAuth

--- src/components/forms/social-auth-form.tsx
+++ src/components/forms/social-auth-form.tsx
@@ -7,6 +7,26 @@ import { signIn } from "next-auth/react";
 import { toast } from "@/hooks/use-toast";
 import { Button } from "@/components/ui/button";
 
+// -----------------------------------------------------------
+
+const authServices = [
+  {
+    id: "github",
+    name: "Github",
+    icon: "icons/github.svg",
+    classIcon: "invert-colors",
+    description: "Log in with Github",
+  },
+  {
+    id: "google",
+    name: "Google",
+    icon: "icons/google.svg",
+    classIcon: "",
+    description: "Log in with Google",
+  },
+];
+// -----------------------------------------------------------
+
 export const SocialAuthForm = () => {
   const buttonClass =
     "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
@@ -29,29 +49,25 @@ export const SocialAuthForm = () => {
       });
     }
   };
+
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
-      <Button className={buttonClass} onClick={() => handleSignIn("github")}>
-        <Image
-          src="icons/github.svg"
-          alt="Github Logo"
-          width={20}
-          height={20}
-          className="invert-colors mr-2.5 object-contain"
-        />
-        <span>Log in with Github</span>
-      </Button>
-
-      <Button className={buttonClass} onClick={() => handleSignIn("google")}>
-        <Image
-          src="icons/google.svg"
-          alt="Google Logo"
-          width={20}
-          height={20}
-          className="mr-2.5 object-contain"
-        />
-        <span>Log in with Google</span>
-      </Button>
+      {authServices.map((service) => (
+        <Button
+          key={service.id}
+          className={buttonClass}
+          onClick={() => handleSignIn(service.id as "github" | "google")}
+        >
+          <Image
+            src={service.icon}
+            alt={`${service.name} Logo`}
+            width={20}
+            height={20}
+            className={`mr-2.5 object-contain ${service.classIcon}`}
+          />
+          <span>{service.description}</span>
+        </Button>
+      ))}
     </div>
   );
 };
