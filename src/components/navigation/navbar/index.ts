bc0b776c65e837c34da9d23d734e59d4617c9a7b
Awaliyatul Hikmah <imadlz15@gmail.com>
2024-11-17 19:45:56 +0700

    implement mobile navigation

--- src/components/navigation/navbar/index.ts
+++ src/components/navigation/navbar/index.ts
@@ -1,1 +1,3 @@
-export * from "./navbar";
+export { default as Navbar } from "./navbar";
+export { default as NavLinks } from "./nav-links";
+export { default as MobileNavigation } from "./mobile-navigation";
--- src/components/navigation/navbar/navbar.tsx
+++ src/components/navigation/navbar/navbar.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Theme from "@/components/navigation/navbar/theme";
+import { MobileNavigation } from "@/components/navigation/navbar";
 
-export const Navbar = () => {
+const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
@@ -22,7 +23,11 @@ export const Navbar = () => {
 
       <div className="flex-between gap-5">
         <Theme />
+
+        <MobileNavigation />
       </div>
     </nav>
   );
 };
+
+export default Navbar;
